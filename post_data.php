27dac7be6882e826f50aa62edef10b205c0f8781
Mateus Lehmkuhl <mateusthiesen@gmail.com>
2018-10-26 15:51:25 -0300

    add mysq connection

--- post_data.php
+++ post_data.php
@@ -1,3 +1,23 @@
 <?php
-echo 'Hello ' . htmlspecialchars($_POST["cod"]) . '!';
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO registros (codigo) VALUES ('" . $_POST["cod"] . "')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
